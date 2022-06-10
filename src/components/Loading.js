022107769dff7c1ea261111c64dda76cb950d2a1
nhkkha00 <74925274+nhkkha00@users.noreply.github.com>
2022-06-10 14:12:44 +0700

    change native-stack to stack

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -15,7 +15,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor:COLORS.mainBg
   }
 });
 
--- src/screens/RootStack.js
+++ src/screens/RootStack.js
@@ -1,21 +1,33 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Easing, Animated } from 'react-native';
+import COLORS from '../colors';
 import DetailScreen from './detail/DetailScreen';
 import MainScreen from './main/MainScreen';
 
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const RootStack = () => {
-
+  
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Main' screenOptions={{headerShown:false}}>
-            <Stack.Screen name='Main' component={MainScreen}/>
-            <Stack.Screen name='Detail' component={DetailScreen}/>
-        </Stack.Navigator>
+      <Stack.Navigator initialRouteName='Main' screenOptions={{ 
+        headerShown: false,
+        ...TransitionPresets.SlideFromRightIOS
+      }}
+      >
+        <Stack.Screen
+          name='Main'
+          component={MainScreen}
+        />
+        <Stack.Screen
+          name='Detail'
+          component={DetailScreen}
+        
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -24,7 +36,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor:COLORS.mainBg
   }
 });
 
